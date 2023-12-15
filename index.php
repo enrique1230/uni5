b3b4807e6fe4238f10270bba133d2fc7a6aca1ff
enrique1230 <153547002+enrique1230@users.noreply.github.com>
2023-12-15 11:19:23 -0600

    index.php

--- index.php
+++ index.php
@@ -21,3 +21,34 @@ $numeroColor = retornarColorHexa(0, 255, 0);  // Green background
 </body>
 </html>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ASCII Table</title>
+</head>
+<body>
+
+<?php
+function obtenerCaracterASCII($valor) {
+    return chr($valor);
+}
+?>
+
+<table border="1">
+    <tr>
+        <th>Decimal</th>
+        <th>Carácter</th>
+    </tr>
+    <?php
+    for ($i = 32; $i <= 127; $i++) {
+        echo "<tr>";
+        echo "<td>$i</td>";
+        echo "<td>" . obtenerCaracterASCII($i) . "</td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
+
+</body>
+</html>
+
